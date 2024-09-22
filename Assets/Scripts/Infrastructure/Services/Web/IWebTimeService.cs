5bc852d42f677b557e4907f671519ae08def5a82
Vlados <ptytb2015@mail.ru>
2024-09-22 06:23:06 +0500

    Created WebTimeService

---
+++ Assets/Scripts/Infrastructure/Services/Web/IWebTimeService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services.Web
+{
+    public interface IWebTimeService : IService
+    {
+        Task<DateTime> FetchTimeAsync();
+    }
+}
---
+++ Assets/Scripts/Infrastructure/Services/Web/WebTimeService.cs
@@ -0,0 +1,32 @@
+﻿using Data;
+using Infrastructure.Services.JSON;
+using System;
+using System.Threading.Tasks;
+using Zenject;
+
+namespace Infrastructure.Services.Web
+{
+    public class WebTimeService : IWebTimeService
+    {
+        private const string URL = "https://yandex.com/time/sync.json";
+
+        private readonly IJsonService _jsonService;
+        private readonly IWebServiceAsync _webServiceAsync;
+
+        [Inject]
+        public WebTimeService(IWebServiceAsync webServiceAsync, IJsonService jsonService)
+        {
+            _jsonService = jsonService;
+            _webServiceAsync = webServiceAsync;
+        }
+
+        public async Task<DateTime> FetchTimeAsync()
+        {
+            var json = await _webServiceAsync.Request(URL);
+
+            TimeData data = _jsonService.Deserialize<TimeData>(json);
+
+            return DateTimeOffset.FromUnixTimeMilliseconds(data.Time).UtcDateTime;
+        }
+    }
+}
