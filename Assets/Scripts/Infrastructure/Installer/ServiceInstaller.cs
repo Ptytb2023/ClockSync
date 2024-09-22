f7148f2bc28694460255f65b9ff25998f6842e73
Vlados <ptytb2015@mail.ru>
2024-09-22 06:23:41 +0500

    Created Installer

---
+++ Assets/Scripts/Infrastructure/Installer/ServiceInstaller.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Services.JSON;
+using Infrastructure.Services.Times;
+using Infrastructure.Services.Web;
+using TimeSynchronizer;
+using Zenject;
+
+namespace Infrastructure.Installer
+{
+    public class ServiceInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesTo<TickService>().AsSingle().NonLazy();
+            Container.BindInterfacesTo<JsonNetService>().AsCached().NonLazy();
+            Container.BindInterfacesTo<WebService>().AsCached().NonLazy();
+            Container.BindInterfacesTo<TimeService>().AsSingle().NonLazy();
+            Container.BindInterfacesTo<WebTimeService>().AsSingle().NonLazy();
+
+            Container.Bind<IWebTimeSynchronizer>().To<WebTimeSynchronizer>().AsSingle().NonLazy();
+        }
+    }
+}
