a3a9499588dce66930e7c72c4a7a0957b5732bcd
Vlados <ptytb2015@mail.ru>
2024-09-22 06:24:57 +0500

    Created TimeConfiguration

---
+++ Assets/Scripts/Clocks/ClockHand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Clocks
+{
+    public class ClockHand : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    {
+        private bool _isDragging = false;
+
+        [SerializeField] private float _rotationStep = 6f;
+
+        private float _currentRotation;
+        public float CurrentRotation => _currentRotation;
+        public event Action UptadeRotation;
+
+        public void OnBeginDrag(PointerEventData eventData) =>
+            _isDragging = true;
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            if (_isDragging)
+                RotateHand(eventData);
+        }
+
+        public void OnEndDrag(PointerEventData eventData) =>
+            _isDragging = false;
+
+        private void RotateHand(PointerEventData eventData)
+        {
+            _currentRotation = transform.localEulerAngles.z;
+
+            if (eventData.delta.y > 0)
+                _currentRotation += _rotationStep;
+            else if (eventData.delta.y < 0)
+                _currentRotation -= _rotationStep;
+
+            _currentRotation = _currentRotation % 360;
+
+            if (_currentRotation < 0) 
+                _currentRotation += 360;
+
+            transform.localEulerAngles = new Vector3(0, 0, _currentRotation);
+
+            UptadeRotation?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Clocks/TimeUnitConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace Clocks
+{
+    public static class TimeUnitConfiguration
+    {
+        public const int HoursPerDay = 24;
+        public const int MinutesPerHour = 60;
+        public const int SecondsPerMinute = 60;
+
+        public const float DegreesPerHour = 360 / 12f;
+        public const float DegreesPerMinute = 360 / 60f;
+        public const float DegreesPerSecond = 360 / 60f;
+    }
+}
